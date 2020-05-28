0fc2db4c40859f3b984ba47bf4be0656ab82e338
sayrajput <40622329+sayrajput@users.noreply.github.com>
2020-05-28 00:23:05 -0700

    Update generators.py

--- udacity dsa nd/generators.py
+++ udacity dsa nd/generators.py
@@ -19,3 +19,24 @@ print(do_something)
 # Now go back to generating more even numbers.
 for i in range(100):
     print(next(my_gen))
+
+    
+
+ 
+
+#output
+0
+2
+4
+6
+8   #my_gen stoped with saving current state
+7  #print(do_something)
+10   #my_gen continued from prev. state
+12
+14
+16
+18
+.
+.
+.
+208

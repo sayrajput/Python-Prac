3594011b2adc76df8c3cc6a31ce88e2d1c9d0c83
sayrajput <40622329+sayrajput@users.noreply.github.com>
2020-06-01 22:11:45 -0700

    Create stringformat.py

---
+++ udacity dsa nd/stringformat.py
@@ -0,0 +1,26 @@
+
+print("Mohammed has {} balloons".format(27))
+
+#Mohammed has 27 balloons
+
+
+animal = "dog"
+action = "bite"
+print("Does your {} {}?".format(animal, action))
+
+#Does your dog bite?
+
+
+
+maria_string = "Maria loves {} and {}"
+print(maria_string.format("math", "statistics"))
+
+#Maria loves math and statistics
+
+
+"""
+Notice how in each example,the number of pairs of curly braces {} you use inside the string
+is the same as the number of replacements you want to make using the values inside format().
+
+More advanced students can learn more about the formal syntax for using the format() string method here.
+"""
